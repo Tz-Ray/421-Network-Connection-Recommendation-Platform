6494237ab7a5c7d8cdea57e4d43aee332c838d81
AlanRQiu <97566592+AlanRQiu@users.noreply.github.com>
2026-03-11 01:00:37 -0700

    fix: forgot to commit App.tsx

--- App.tsx
+++ App.tsx
@@ -3,15 +3,28 @@ import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import DashboardScreen from './screens/DashboardScreen';
+import ProtectedRoute from './ProtectedRoute';
+import PublicRoute from './PublicRoute';
 
 const App: React.FC = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/register" element={<RegisterScreen />} />
-        <Route path="/dashboard" element={<DashboardScreen />} />
+        
+        {/* Public Routes: Redirect to dashboard if already logged in */}
+        <Route path="/login" element={<PublicRoute><LoginScreen /></PublicRoute>} />
+        <Route path="/register" element={<PublicRoute><RegisterScreen /></PublicRoute>} />
+        
+        {/* Protected Route: Redirect to login if NOT logged in */}
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute>
+              <DashboardScreen />
+            </ProtectedRoute>
+          } 
+        />
       </Routes>
     </HashRouter>
   );
